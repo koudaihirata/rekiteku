4637977cd0a1530a0c9932e9ee22df8e9ff4a72f
koudaihirata <2230051@ecc.ac.jp>
2024-07-08 19:47:02 +0900

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { css } from "../../styled-system/css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Profile from "@/components/Profile";
+import FirstView from "@/components/FirstView";
+import LeadText from "@/components/LeadText";
+import Plans from "@/components/plans";
 import Notes from "@/components/Notes";
+import Footer from "@/components/Footer";
 
-export default async function Home() {
-
+export default function Home() {
   return (
     <main>
+      <FirstView/>
+      <LeadText/>
+      <Plans />
+      <Profile/>
       <Notes/>
+      <Footer/>
     </main>
   );
 }
+
