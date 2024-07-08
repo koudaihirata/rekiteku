24967f36117215347c9891890439e76f65324027
koudaihirata <2230051@ecc.ac.jp>
2024-07-08 11:40:43 +0900

    Footerコンポーネント完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { css } from "../../styled-system/css";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    <main>
+      <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+      <Footer/>
+    </main>
   );
 }
