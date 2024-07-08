4c96c35644f1dfada81d8a0b0fdad30aaca6525b
koudaihirata <2230051@ecc.ac.jp>
2024-07-08 09:49:12 +0900

    FirstView完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { css } from "../../styled-system/css";
+import FirstView from "@/components/FirstView";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    <main>
+      <FirstView/>
+      <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    </main>
   );
 }
