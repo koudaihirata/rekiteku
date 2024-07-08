7714d54fec1c3edbea059415db7fb33bd3df674d
koudaihirata <2230051@ecc.ac.jp>
2024-07-08 10:21:28 +0900

    Profileコンポーネント完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { css } from "../../styled-system/css";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    <main>
+      <Profile/>
+      <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    </main>
   );
 }
