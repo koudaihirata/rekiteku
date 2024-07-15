f61b338867bdd4f4af9be1644949f8e5008d09fc
小淵　颯太 <>
2024-07-15 10:13:47 +0900

    font変更

--- src/components/FirstView.tsx
+++ src/components/FirstView.tsx
@@ -1,14 +1,35 @@
 import Image from "next/image";
 import { css } from "../../styled-system/css";
 
-
 export default function FirstView() {
-    return(
-        <>
-            <div className={css({width:'100%',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',gap:'50px'})}>
-                <Image src='/FirstViewImg.svg' alt="FirstView" width={600} height={450} />
-                <h2 className={css({fontSize:'32px'})}>あなたの好きな歴史の軌跡を辿る</h2>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div
+				className={css({
+					width: "100%",
+					height: "100vh",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flexDirection: "column",
+					gap: "50px",
+				})}
+			>
+				<Image
+					src="/FirstViewImg.svg"
+					alt="FirstView"
+					width={600}
+					height={450}
+				/>
+				<h2
+					className={css({
+						fontSize: "32px",
+						fontFamily: '"Shippori Mincho B1", serif',
+					})}
+				>
+					あなたの好きな歴史の軌跡を辿る
+				</h2>
+			</div>
+		</>
+	);
 }
